64787cbb5a627a2cecea9f9791efe0d4cfbbd5cf
savaskelez <savaskelez@gmail.com>
2020-12-01 23:27:19 +0300

    odevler

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MovieList from './components/FilterableMoviesList/movieList';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Film Listesi</h1>
+      <MovieList / >
+    </div>
+  );
+}
+
+export default App;
